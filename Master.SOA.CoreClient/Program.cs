d9b59096ddc1a252b91e5a87968b5f44bfa3a3c6
Luka Radovanovic <luka.radovanovic@teletrader.com>
2020-11-24 19:49:34 +0100

    working in folder structure

---
+++ Master.SOA.CoreClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Master.SOA.CoreClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
